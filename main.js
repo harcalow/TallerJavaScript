9e83dc51017ad800d880f50833f685b1330255aa
Harold Calderon <harold.1408@hotmail.com>
2021-09-07 08:48:27 -0500

    Creacion del objeto rectangulo

--- main.js
+++ main.js
@@ -20,6 +20,33 @@
  }
 })();
 
+
+(function(){
+    self.Bar = function(x,y,width,height,board){
+     this.x = x;
+     this.y = y;
+     this.width = width;
+     this.height = height;
+     this.board = board;
+     this.board.bars.push(this);
+     this.kind = "rectangle";
+     this.speed = 10;
+    }
+   
+    self.Bar.prototype = {
+     down: function(){
+      this.y += this.speed;
+   
+     },
+     up: function(){
+      this.y -= this.speed;
+     },
+     toString: function(){
+      return "x: "+ this.x +"y: "+ this.y ;
+     }
+    }
+   })();
+
 (function(){
     self.BoardView = function(canvas,board){
      this.canvas = canvas;
@@ -28,12 +55,35 @@
      this.board = board;
      this.cxt = canvas.getContext("2d");
     }
+    self.BoardView.prototype={
+        draw: function(){
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+                draw(this.cxt,el);
+               };        
+
+        }
+    }
+    function draw(cxt,element){ 
+        if (element !==null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                   cxt.fillRect(element.x,element.y,element.width,element.height);
+                break;
+               }
+        }
+
+    }
+
+
 })();
 
 
 window.addEventListener("load",main);
 function main() {
     var board =new Board(800,400);
+    var bar =new Bar(20,100,40,100,board);
     var canvas =document.getElementById('canvas');
     var board_view=new BoarView(canvas,board);
+    board_view.draw();
 }
